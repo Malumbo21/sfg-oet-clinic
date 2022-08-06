970264fb57eedf537318f2ccb2f45dbe5ebe3036
Malumbo Sinkamba <Malumbo@chrilantech.com>
2022-08-06 19:06:54 +0200

    Added petTypes to bootstrap. closes #33

--- pet-clinic-web/src/main/java/com/osyrs/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/osyrs/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.osyrs.sfgpetclinic.bootstrap;
 
 import com.osyrs.sfgpetclinic.model.Owner;
+import com.osyrs.sfgpetclinic.model.PetType;
 import com.osyrs.sfgpetclinic.model.Vet;
 import com.osyrs.sfgpetclinic.services.OwnerService;
+import com.osyrs.sfgpetclinic.services.PetTypeService;
 import com.osyrs.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,45 +15,42 @@ import org.springframework.web.bind.annotation.RestController;
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        var owner1 = new Owner();
-        owner1.setFirstName("Malumbo");
-        owner1.setLastName("Sinkamba");
-
-        ownerService.save(owner1);
-
-        var owner2 = new Owner();
-        owner2.setFirstName("Kaluwe");
-        owner2.setLastName("Mutemwa");
-        /* DataSource dataSource = DataSourceBuilder.create()
-                .url("")
-                .username("")
-                .password("")
-                .driverClassName("")
-                .build();*/
-        ownerService.save(owner2);
-
+        var owner1 = saveOwner("Malumbo","Sinkamba");
+        var owner2 = saveOwner("Kaluwe","Mutemwa");
         System.out.println("Loaded Owners.......");
-
-        var vet1 = new Vet();
-        vet1.setFirstName("Lenganji");
-        vet1.setLastName("Sinkamba");
-
-        vetService.save(vet1);
-
-        var vet2 = new Vet();
-        vet2.setFirstName("Suwilanji");
-        vet2.setLastName("Sinkamba");
-
-        vetService.save(vet2);
-
+        var vet1 = saveVet("Lenganji","Sinkamba");
+        var vet2 = saveVet("Suwilanji","Sinkamba");
         System.out.println("Loaded Vets.......");
+        var dogType = savePetType("Dog");
+        var catType = savePetType("Cat");
+        System.out.println("Loaded PetTypes.......");
+    }
+
+    private Owner saveOwner(String firstName, String lastName) {
+        var owner = new Owner();
+        owner.setFirstName(firstName);
+        owner.setLastName(lastName);
+        return ownerService.save(owner);
+    }
+    private Vet saveVet(String firstName, String lastName) {
+        var owner = new Vet();
+        owner.setFirstName(firstName);
+        owner.setLastName(lastName);
+        return vetService.save(owner);
+    }
+    private PetType savePetType(String name){
+        var petType= new PetType();
+        petType.setName(name);
+        return petTypeService.save(petType);
     }
 }
