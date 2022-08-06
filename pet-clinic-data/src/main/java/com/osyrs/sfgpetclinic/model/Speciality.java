554d7ed5bb21fc8ce1061cbe70e6ce21bfb34cd9
Malumbo Sinkamba <Malumbo@chrilantech.com>
2022-08-06 14:26:16 +0200

    Created Vet Speciality Entity and Associated Speciality  to Vet. closes #28

---
+++ pet-clinic-data/src/main/java/com/osyrs/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package com.osyrs.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity{
+    String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
