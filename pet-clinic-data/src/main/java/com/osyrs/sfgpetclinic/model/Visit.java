4eb387075c7e223c10c48193f0c8417dd859a80c
Malumbo Sinkamba <Malumbo@chrilantech.com>
2022-08-08 22:24:43 +0200

    Added Visit Entity. closes #39

--- pet-clinic-data/src/main/java/com/osyrs/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/osyrs/sfgpetclinic/model/Visit.java
@@ -1,10 +1,17 @@
 package com.osyrs.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
@@ -30,4 +37,5 @@ public class Visit extends BaseEntity{
     public void setPet(Pet pet) {
         this.pet = pet;
     }
+
 }
