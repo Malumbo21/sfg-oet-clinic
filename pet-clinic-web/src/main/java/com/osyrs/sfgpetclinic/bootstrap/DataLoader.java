a5c193636dea7036b985335a420377e0acc826dc
Malumbo Sinkamba <Malumbo@chrilantech.com>
2022-08-11 22:01:48 +0200

    Added Visit Data in startup bootstrap. Closes #49

--- pet-clinic-web/src/main/java/com/osyrs/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/osyrs/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.osyrs.sfgpetclinic.bootstrap;
 
 import com.osyrs.sfgpetclinic.model.*;
-import com.osyrs.sfgpetclinic.services.OwnerService;
-import com.osyrs.sfgpetclinic.services.PetTypeService;
-import com.osyrs.sfgpetclinic.services.SpecialtyService;
-import com.osyrs.sfgpetclinic.services.VetService;
+import com.osyrs.sfgpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +16,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -42,14 +41,17 @@ public class DataLoader implements CommandLineRunner {
 
         var owner1 = createOwner("Malumbo","Sinkamba","Off Ring Road","Lusaka","+260972139603");
         var pet1 = assignPetToOwner(owner1,"King",dogType, LocalDate.of(2020,2,2));
-        var pet2 = assignPetToOwner(owner1,"Nova",dogType, LocalDate.of(2019,12,18));
+        var pet2 = assignPetToOwner(owner1,"Nova",catType, LocalDate.of(2019,12,18));
         ownerService.save(owner1);
         var owner2 = createOwner("Kaluwe","Mutemwa","24 Salama","Lusaka","+260977327251");
         var pet3 = assignPetToOwner(owner2,"Sugar",dogType, LocalDate.of(2019,12,18));
-        var pet4 = assignPetToOwner(owner2,"Spike",dogType, LocalDate.of(2019,12,18));
+        var pet4 = assignPetToOwner(owner2,"Spike",catType, LocalDate.of(2019,12,18));
         ownerService.save(owner2);
         System.out.println("Loaded Owners.......");
-
+        var visitDescription = "Sneezing cat";
+        var visitDate = LocalDate.now();
+        var visit = createVisit(pet1, visitDescription, visitDate);
+        visitService.save(visit);
         Specialty radiology = getSpecialty("Radiology");
         var surgery = getSpecialty("Surgery");
         var dentistry = getSpecialty("Dentistry");
@@ -58,6 +60,14 @@ public class DataLoader implements CommandLineRunner {
         var vet1 = saveVet("Lenganji","Sinkamba",surgery);
         var vet2 = saveVet("Suwilanji","Sinkamba",radiology);
         System.out.println("Loaded Vets.......");
+    }
+
+    private Visit createVisit(Pet pet1, String visitDescription, LocalDate visitDate) {
+        var visit = new Visit();
+        visit.setDate(visitDate);
+        visit.setPet(pet1);
+        visit.setDescription(visitDescription);
+        return visit;
     }
 
     private Specialty getSpecialty(String description) {
